28f628a32cd982a8d2ae4c74f2e96ac9f731dfc4
Piotr Ulanowski <pioula111@gmail.com>
2023-05-17 23:13:05 +0200

    c

--- main.py
+++ main.py
@@ -12,10 +12,10 @@ args = parser.parse_args()
 
 NUMBER_OF_EPOCHS = args.ep
 import tensorflow as tf
-device_name = tf.test.gpu_device_name()
-if device_name != '/device:GPU:0':
-  raise SystemError('GPU device not found')
-print('Found GPU at: {}'.format(device_name))
+# device_name = tf.test.gpu_device_name()
+# if device_name != '/device:GPU:0':
+#   raise SystemError('GPU device not found')
+# print('Found GPU at: {}'.format(device_name))
 base_path = args.path
 
 
