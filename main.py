19d6c5a5724218f99e435299103ed52d78cd7c0c
Piotr Ulanowski <pioula111@gmail.com>
2023-05-17 15:17:28 +0200

    sthm

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 #%%
+NUMBER_OF_IMAGES = 10000
+base_path = './'
+#%%
 import argparse
 
 parser = argparse.ArgumentParser(description='Example of command line arguments')
 parser.add_argument('--path', type=str, help='an argument')
-parser.add_argument('--img', type=int, help='an argument')
 parser.add_argument('--ep', type=int, help='an argument')
 args = parser.parse_args()
 
@@ -11,52 +13,51 @@ NUMBER_OF_IMAGES = args.img
 NUMBER_OF_EPOCHS = args.ep
 
 base_path = args.path
-#%% 
-import pandas as pd
-df = pd.read_csv(f'{base_path}BigDataCup2022/S1/train/train.csv')
-df.head()
-
 #%%
+def cycle_shift(ds):
+    dataset0 = ds.enumerate()
+    dataset1 = dataset0.filter(lambda x, _: x == 0)
+    dataset2 = dataset0.filter(lambda x, _: x != 0)
+    dataset3 = dataset2.concatenate(dataset1)
+    dataset4 = dataset3.map(lambda _, y: y)
+    return dataset4
+#%%
+import tensorflow as tf
+from tensorflow import keras
 
-# df has columns id, input_path, encoded_path which contains id path of one image and a path to its corresponding encoded version
-# Import first 100 images into the numpy array
-import numpy as np
-import os
-import cv2
-NUMBER_OF_IMAGES = NUMBER_OF_IMAGES
+input = keras.utils.image_dataset_from_directory(
+    directory=f'{base_path}BigDataCup2022/S1/train/input',
+    labels=None,
+    label_mode=None,
+    image_size=(512,512),
+    shuffle=False
+    )
+#%%
+encoded = keras.utils.image_dataset_from_directory(
+    directory=f'{base_path}BigDataCup2022/S1/train/enc',
+    labels=None,
+    label_mode=None,
+    image_size=(512,512),
+    shuffle=False
+)
+#%%
+zipped = tf.data.Dataset.zip((input, encoded))
+good_labels = zipped.map(lambda x, y: (tf.concat((x, y), axis=3), [1]))
 
-images = np.zeros((NUMBER_OF_IMAGES * 2, 2, 512, 512, 3), dtype=np.uint8)
+encoded_shift = cycle_shift(encoded)
+zipped_bad = tf.data.Dataset.zip((input, encoded_shift))
+bad_labels = zipped_bad.map(lambda x, y: (tf.concat((x, y), axis=3), [0]))
+X = good_labels.concatenate(bad_labels).map(lambda x, y: (x / 255., y))
+#%%
+def add_labels(image, label):
+    # Your custom label logic here
+    return image, tf.expand_dims(label, axis=-1)
 
-# Write a for which will loop over first 100 images and save them into a numpy array
-for i in range(NUMBER_OF_IMAGES):
-    # Read the image
-    img = cv2.imread(f'{base_path}{df.iloc[i,1]}')
-    encoded = cv2.imread(f'{base_path}{df.iloc[i,2]}')
-    # Save the image into the numpy array
-    images[i][0] = img
-    images[i][1] = encoded
+dataset = X
 
-images.shape
-# %% no classes
-import random
-for i in range(NUMBER_OF_IMAGES):
-    img = images[i][0]
-    i2 = i
-    while i2 == i:
-        i2 = random.randint(0, NUMBER_OF_IMAGES - 1)
-    enc = images[i2][1]
-    images[i + NUMBER_OF_IMAGES][0] = img
-    images[i + NUMBER_OF_IMAGES][1] = enc
-#%% labels
-labels = np.array([1] * NUMBER_OF_IMAGES + [0] * NUMBER_OF_IMAGES)
-#%% six channels
-X = np.array([cv2.merge((img[0][:,:,0], img[0][:,:,1], img[0][:,:,2], img[1][:,:,0], img[1][:,:,1], img[1][:,:,2])) for img in images], dtype=float)
-X /= 255.
-#%% Train validation  split
-from sklearn.model_selection import train_test_split
-
-X_train, X_val, y_train, y_val = train_test_split(X, labels, test_size=0.2, random_state=42)
-# %%
+new_dataset = dataset.map(lambda x, y: add_labels(x, y))
+new_dataset
+#%%
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers
@@ -71,22 +72,21 @@ with tf.device('/device:GPU:0'):
     model.compile(optimizer=keras.optimizers.Adam(), loss="binary_crossentropy", metrics=["accuracy"])
     print("fitting")
     # Fit the model to the data
-    model.fit(
-        x=X_train,
-        y=y_train,
-        epochs=NUMBER_OF_EPOCHS,
-        validation_data=(X_val, y_val),
-        batch_size=2
-    )
 
-    predictions = model.predict(X_val)
-    print(predictions)
+    model.fit(X, epochs=5)
 
-    # round predictions
-    predictions = np.round(predictions)
+    # model.fit(
+    #     X, epochs=10
+    # )
 
-    accuracy = tf.keras.metrics.Accuracy()
-    print(accuracy(predictions, y_val))
+    # predictions = model.predict(X_val)
+    # print(predictions)
+
+    # # round predictions
+    # predictions = np.round(predictions)
+
+    # accuracy = tf.keras.metrics.Accuracy()
+    # print(accuracy(predictions, y_val))
 # %%
 
     # # Data augmentation
